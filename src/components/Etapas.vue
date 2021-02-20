40eb46d6690e6b8bde064252accc6aaa57226340
Borba <vinicius_fuhr-borba@estudante.sc.senai.br>
2021-02-20 10:02:01 -0300

    Desenvolvimento tela de etapas, acrescentando verificação para apresentar elementos

--- src/components/Etapas.vue
+++ src/components/Etapas.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
+    <div class="link">
+      <a class="itens-header" href="/">Voltar</a>
+    </div>
     <div class="etapas">
       <div class="etapa">
         <h3>Etapa 1</h3>
         <h4>Status:</h4>
-        <p>Em andamento</p>
+        <div v-if="this.andamentoEtapa1">
+          <p>Em Andamento</p>
+        </div>
+        <div v-else>
+          <p>Finalizado</p>
+        </div>
         <button
           @click="finalizarEtapa1"
           class="button"
@@ -16,7 +24,15 @@
       <div class="etapa">
         <h3>Etapa 2</h3>
         <h4>Status:</h4>
-        <p>Aguarde</p>
+        <div v-if="!this.andamentoEtapa2 && this.andamentoEtapa1">
+          <p>Aguarde</p>
+        </div>
+        <div v-if="this.andamentoEtapa2">
+          <p>Em Andamento</p>
+        </div>
+        <div v-if="!this.andamentoEtapa2 && !this.andamentoEtapa1">
+          <p>Finalizado</p>
+        </div>
         <button
           @click="finalizarEtapa2"
           class="button"
@@ -40,10 +56,16 @@ export default {
       allParticipantesSala1: [],
       allParticipantesSala2: [],
       aux: [],
+      allEtapas: [],
+      andamentoEtapa1: false,
+      andamentoEtapa2: false,
     };
   },
   methods: {
     finalizarEtapa1: function () {
+      console.log(this.andamentoEtapa1);
+      console.log(this.andamentoEtapa2);
+      console.log(this.allEtapas);
       if (this.verificacaoBtnEtapa1 == true) {
         for (var i = 0; i < this.allParticipantes.length; i++) {
           if (this.allParticipantes[i].sala1 == 1) {
@@ -75,23 +97,58 @@ export default {
         this.allParticipantes = this.allParticipantesSala1.concat(
           this.allParticipantesSala2
         );
+        this.allEtapas.forEach((el) => {
+          if (el.id == 1) {
+            el.situacao = 0;
+          }
+          if (el.id == 2) {
+            el.situacao = 1;
+          }
+        });
         this.allParticipantes.forEach((el) => {
           axios.put("http://localhost:55560/api/participante/" + el.id, {
             sala1: el.sala1,
             sala2: el.sala2,
           });
         });
+        this.allEtapas.forEach((el) => {
+          axios.put("http://localhost:55560/api/etapas/" + el.id, {
+            situacao: el.situacao,
+          });
+        });
         this.allParticipantes = [];
         this.allParticipantesSala1 = [];
         this.allParticipantesSala2 = [];
         this.aux = [];
-        this.verificacaoBtnEtapa1 = false;
-        this.verificacaoBtnEtapa2 = true;
+        window.setTimeout(function reload() {
+          location.reload();
+        }, 2000);
       }
     },
     finalizarEtapa2: function () {
       if (this.verificacaoBtnEtapa2 == true) {
-        this.verificacaoBtnEtapa2 = false;
+        this.allEtapas.forEach((el) => {
+          if (el.id == 1) {
+            el.situacao = 0;
+          }
+          if (el.id == 2) {
+            el.situacao = 0;
+          }
+        });
+        this.allParticipantes.forEach((el) => {
+          axios.put("http://localhost:55560/api/participante/" + el.id, {
+            sala1: el.sala1,
+            sala2: el.sala2,
+          });
+        });
+        this.allEtapas.forEach((el) => {
+          axios.put("http://localhost:55560/api/etapas/" + el.id, {
+            situacao: el.situacao,
+          });
+        });
+        window.setTimeout(function reload() {
+          location.reload();
+        }, 2000);
       }
     },
   },
@@ -99,6 +156,26 @@ export default {
     axios
       .get("http://localhost:55560/api/participante")
       .then((resp) => (this.allParticipantes = resp.data));
+    axios.get("http://localhost:55560/api/etapas").then((resp) => {
+      this.allEtapas = resp.data;
+      for (var i3 = 0; i3 < this.allEtapas.length; i3++) {
+        if (this.allEtapas[i3].id == 1) {
+          this.andamentoEtapa1 = this.allEtapas[i3].situacao;
+          if (this.andamentoEtapa1 == false) {
+            this.verificacaoBtnEtapa1 = false;
+          }
+        }
+        if (this.allEtapas[i3].id == 2) {
+          this.andamentoEtapa2 = this.allEtapas[i3].situacao;
+          if (this.andamentoEtapa2 == true) {
+            this.verificacaoBtnEtapa2 = true;
+          }
+          if (this.andamentoEtapa2 == false) {
+            this.verificacaoBtnEtapa2 = false;
+          }
+        }
+      }
+    });
   },
 };
 </script>
